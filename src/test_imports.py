479f02059f9bf137e8b34b9e2a5e22fdc70fdcfa
Varun <varunkumarm192@gmail.com>
2026-07-02 10:59:39 +0530

    Month 2 Week 1: cleaned up src/ folder and added requirements.txt

--- src/test_imports.py
+++
@@ -1,9 +0,0 @@
-import langchain
-import chromadb
-import sentence_transformers
-import streamlit
-import pypdf
-from langchain_groq import ChatGroq
-from dotenv import load_dotenv
-
-print("All imports successful!")
